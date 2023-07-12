599822f7319b5522e6dea0df3cb0facecd0be682
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-07-12 14:54:51 +0200

    Finishing Banner Component

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./Banner.module.css";
 
 const Banner = ({ title, subtitle, imgURL }) => {
@@ -6,11 +7,34 @@ const Banner = ({ title, subtitle, imgURL }) => {
     };
 
     return (
-        <div>
-            <h3>{title}</h3>
-            <h3>{subtitle}</h3>
-            <button onClick={handleOnPlay}>Play</button>
+        <div className={styles.container}>
+            <div className={styles.leftWrapper}>
+                <div className={styles.left}>
+                    <div className={styles.nseriesWrapper}>
+                        <p className={styles.firstLetter}>N</p>
+                        <p className={styles.series}>S E R I E S</p>
+                    </div>
+                    <h3 className={styles.title}>{title}</h3>
+                    <h3 className={styles.subTitle}>{subtitle}</h3>
+                    <div className={styles.playBtnWrapper}>
+                        <button
+                            className={styles.btnWithIcon}
+                            onClick={handleOnPlay}
+                        >
+                            <Image
+                                src="/static/play_arrow.svg"
+                                alt="Play Icon"
+                                width="32"
+                                height="32"
+                            />
+                            <span className={styles.playText}> Play</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
             <div
+                className={styles.bannerImg}
                 style={{
                     backgroundImage: `url(${imgURL})`,
                     width: "100%",
@@ -25,3 +49,5 @@ const Banner = ({ title, subtitle, imgURL }) => {
 };
 
 export default Banner;
+
+// className={styles.}
