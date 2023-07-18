b610cca65161f3650c77058a39484df357e74230
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-07-18 11:44:11 +0200

    NavBar Component

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,32 +1,79 @@
+import Link from "next/link";
 import styles from "./NavBar.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Image from "next/image";
 
 const NavBar = ({ username }) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const router = useRouter();
+
+    const handleOnClickHome = (e) => {
+        e.preventDefault();
+        router.push("/");
+    };
+
+    const handleOnClickMyList = (e) => {
+        e.preventDefault();
+        router.push("/browse/my-list");
+    };
+
+    const handleShowDropdown = (e) => {
+        e.preventDefault();
+        setShowDropdown(!showDropdown);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                <a href="/" className={styles.logoLink}>
-                    <div className={styles.logoWrapper}>Netflix</div>
-                </a>
+                <Link href="/" className={styles.logoLink}>
+                    <div className={styles.logoWrapper}>
+                        <Image
+                            src={"/static/netflix.svg"}
+                            alt="Netflix Logo"
+                            width="128"
+                            height="34"
+                        />
+                    </div>
+                </Link>
                 <ul className={styles.navItems}>
-                    <li className={styles.navItem}>Home</li>
-                    <li className={styles.navItem2}>My List</li>
+                    <li className={styles.navItem} onClick={handleOnClickHome}>
+                        Home
+                    </li>
+                    <li
+                        className={styles.navItem2}
+                        onClick={handleOnClickMyList}
+                    >
+                        My List
+                    </li>
                 </ul>
+
                 <nav className={styles.navContainer}>
                     <div>
-                        <button className={styles.usernameBtn}>
+                        <button
+                            className={styles.usernameBtn}
+                            onClick={handleShowDropdown}
+                        >
                             <p className={styles.username}>{username}</p>
-                            {/* ICON */}
+                            <Image
+                                src={"/static/expand_more.svg"}
+                                alt="Expand More Icon"
+                                width="24"
+                                height="24"
+                            />
                         </button>
                     </div>
 
-                    <div className={styles.navDropdown}>
-                        <div>
-                            <a href="/" className={styles.linkName}>
-                                Sign Out
-                            </a>
-                            <div className={styles.lineWrapper}></div>
+                    {showDropdown && (
+                        <div className={styles.navDropdown}>
+                            <div>
+                                <Link href="/login" className={styles.linkName}>
+                                    Sign Out
+                                </Link>
+                                <div className={styles.lineWrapper}></div>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </nav>
             </div>
         </div>
