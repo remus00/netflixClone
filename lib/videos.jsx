0608f1f70ae3d72fc6faf16bdd3f91cce7961fdf
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-08-01 11:21:32 +0200

    Implementing popular Videos

--- lib/videos.jsx
+++ lib/videos.jsx
@@ -3,7 +3,7 @@ export const getCommonVideos = async (url) => {
     try {
         const BASE_URL = "youtube.googleapis.com/youtube/v3";
         const response = await fetch(
-            `https://${BASE_URL}/${url}&key=${YOUTUBE_API_KEY}`,
+            `https://${BASE_URL}/${url}&maxResults=25&key=${YOUTUBE_API_KEY}`,
         );
         const data = await response.json();
 
@@ -28,10 +28,12 @@ export const getCommonVideos = async (url) => {
 };
 
 export const getVideos = async (searchQuery) => {
-    const URL = `search?part=snippet&maxResults=25&q=${searchQuery}`;
+    const URL = `search?part=snippet&q=${searchQuery}`;
     return getCommonVideos(URL);
 };
 
-/* 
-`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`
-`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${YOUTUBE_API_KEY}` */
+export const getPopularVideos = async () => {
+    const URL =
+        "videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US";
+    return getCommonVideos(URL);
+};
--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,32 @@ import styles from "../styles/Home.module.css";
 import Banner from "@/components/Banner/Banner";
 import NavBar from "@/components/NavBar/NavBar";
 import SectionCards from "@/components/Card/SectionCards";
-import { getVideos } from "@/lib/videos";
+import { getPopularVideos, getVideos } from "@/lib/videos";
 
 export async function getServerSideProps() {
     const disneyVideos = await getVideos("disney trailer");
     const travelVideos = await getVideos("travel");
     const productivityVideos = await getVideos("productivity");
-    // const popularVideos = await getVideos();
-    return { props: { disneyVideos, travelVideos, productivityVideos } };
+    const popularVideos = await getPopularVideos();
+    return {
+        props: {
+            disneyVideos,
+            travelVideos,
+            productivityVideos,
+            popularVideos,
+        },
+    };
 }
 
 export default function Home({
     disneyVideos,
     productivityVideos,
     travelVideos,
+    popularVideos,
 }) {
     console.log("DA HOME", disneyVideos);
     return (
-        <>
+        <div className={styles.container}>
             <Head>
                 <title>Netflix Clone</title>
                 <meta name="description" content="Netflix Clone" />
@@ -31,35 +39,38 @@ export default function Home({
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <NavBar username="remus.burlacu00@gmail.com" />
-            <Banner
-                title="Clifford the red dog"
-                subtitle="He's a very cute dog"
-                imgURL="/static/clifford.webp"
-            />
-
-            <div className={styles.sectionWrapper}>
-                <SectionCards
-                    title="Disney"
-                    videos={disneyVideos}
-                    size="large"
+            <div className={styles.main}>
+                <NavBar username="remus.burlacu00@gmail.com" />
+                <Banner
+                    title="Clifford the red dog"
+                    subtitle="He's a very cute dog"
+                    imgURL="/static/clifford.webp"
                 />
 
-                <SectionCards title="Travel"
-                    videos={travelVideos}
-                    size="small"
-                />
-                <SectionCards
-                    title="Productivity"
-                    videos={productivityVideos}
-                    size="medium"
-                />
-                <SectionCards
-                    title="Popular"
-                    videos={disneyVideos}
-                    size="small"
-                />
+                <div className={styles.sectionWrapper}>
+                    <SectionCards
+                        title="Disney"
+                        videos={disneyVideos}
+                        size="large"
+                    />
+
+                    <SectionCards
+                        title="Travel"
+                        videos={travelVideos}
+                        size="small"
+                    />
+                    <SectionCards
+                        title="Productivity"
+                        videos={productivityVideos}
+                        size="medium"
+                    />
+                    <SectionCards
+                        title="Popular"
+                        videos={popularVideos}
+                        size="small"
+                    />
+                </div>
             </div>
-        </>
+        </div>
     );
 }
