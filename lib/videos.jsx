d7e2e83b4373702dcb5be7eb3459ea281d921eab
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-08-01 09:33:33 +0200

    Handling error in Videos

--- lib/videos.jsx
+++ lib/videos.jsx
@@ -1,18 +1,28 @@
-import videosData from "../data/videos.json";
-
 export const getVideos = async (searchQuery) => {
     const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
-    const response = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`,
-    );
-    const data = await response.json();
+    try {
+        const response = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`,
+        );
+        const data = await response.json();
 
-    return data?.items.map((item) => {
-        return {
-            title: item.snippet.title,
-            imgURL: item.snippet.thumbnails.high.url,
-            id: item?.id?.videoId || null,
-        };
-    });
+        if (data?.error) {
+            console.error("Youtube API error!", data.error);
+            return [];
+        }
+
+        return data?.items.map((item) => {
+            // console.log({ id: item.id });
+            const id = item.id?.videoId || item.id;
+            return {
+                title: item.snippet.title,
+                imgURL: item.snippet.thumbnails.high.url,
+                id,
+            };
+        });
+    } catch (error) {
+        console.log("Something went wrong with the video library", error);
+        return [];
+    }
 };
 // disney%20trailer
