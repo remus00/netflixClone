b3dc9cc55e17c137bb8e1fdf935fc293b7e37459
Marian Burlacu <marian.burlacu@edu.unito.it>
2023-08-01 09:24:18 +0200

    adding other Video Category section

--- lib/videos.jsx
+++ lib/videos.jsx
@@ -1,9 +1,9 @@
 import videosData from "../data/videos.json";
 
-export const getVideos = async () => {
+export const getVideos = async (searchQuery) => {
     const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
     const response = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=disney%20trailer&key=${YOUTUBE_API_KEY}`,
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchQuery}&key=${YOUTUBE_API_KEY}`,
     );
     const data = await response.json();
 
@@ -15,3 +15,4 @@ export const getVideos = async () => {
         };
     });
 };
+// disney%20trailer
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,18 @@ import SectionCards from "@/components/Card/SectionCards";
 import { getVideos } from "@/lib/videos";
 
 export async function getServerSideProps() {
-    const disneyVideos = await getVideos();
-    console.log(disneyVideos);
-    return { props: { disneyVideos } };
+    const disneyVideos = await getVideos("disney trailer");
+    const travelVideos = await getVideos("travel");
+    const productivityVideos = await getVideos("productivity");
+    // const popularVideos = await getVideos();
+    return { props: { disneyVideos, travelVideos, productivityVideos } };
 }
 
-export default function Home({ disneyVideos }) {
+export default function Home({
+    disneyVideos,
+    productivityVideos,
+    travelVideos,
+}) {
     console.log("DA HOME", disneyVideos);
     return (
         <>
@@ -39,10 +45,19 @@ export default function Home({ disneyVideos }) {
                     size="large"
                 />
 
+                <SectionCards title="Travel"
+                    videos={travelVideos}
+                    size="small"
+                />
                 <SectionCards
                     title="Productivity"
-                    videos={disneyVideos}
+                    videos={productivityVideos}
                     size="medium"
+                />
+                <SectionCards
+                    title="Popular"
+                    videos={disneyVideos}
+                    size="small"
                 />
             </div>
         </>
